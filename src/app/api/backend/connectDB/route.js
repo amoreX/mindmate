cad4c398568c236d729f30f0369f3b62f6bd270d
nihal <rahmannihal72@gmail.com>
2025-03-09 16:53:59 +0530

    toast-added

--- src/app/api/backend/connectDB/route.js
+++ src/app/api/backend/connectDB/route.js
@@ -1,16 +1,5 @@
 import mongoose from "mongoose";
 
-export async function connectdb() {
-	await mongoose.connect(
-		"mongodb+srv://nihal:mdnihalrahman@2005@mindmate.llti2.mongodb.net/?retryWrites=true&w=majority&appName=mindmate"
-	);
-	const db = mongoose.connection;
-	db.on("error", console.error.bind(console, "connection error:"));
-	db.once("open", function () {
-		console.log("Connected to MongoDB");
-	});
-}
-
 const connectDB = async () => {
 	try {
 		await mongoose.connect(
